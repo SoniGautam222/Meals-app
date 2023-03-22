b97fc785de660a95cfe84a48bcce533daba43f61
Soni Gautam <112372150+SoniGautam222@users.noreply.github.com>
2023-03-22 20:07:10 +0530

    Add files via upload

---
+++ fav.js
@@ -0,0 +1,46 @@
+let display = document.getElementById("meal-list");
+
+let data = JSON.parse(localStorage.getItem("Mealsid"));
+// console.log("this is dtaa" , data);
+
+data.map(async (item) => {
+  let res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${item}`
+  );
+  let data = await res.json();
+  display.innerHTML += `
+    <li id="${data.meals[0].idMeal}" class="list">
+    <div id="list-part">
+            <div class="img"><img src="${data.meals[0].strMealThumb}"/>
+            </div>
+            <div class="name"> ${data.meals[0].strMeal}
+            </div>
+            <button class="fav-btn" onclick="removefavItem(${data.meals[0].idMeal})">favourite</button>
+            </div>
+    </li>`;
+});
+
+let fav=[];
+function removefavItem(id) {
+  // Retrieve Mealsid data from local storage
+  let Mealsid = JSON.parse(localStorage.getItem("Mealsid")) || [];
+
+ // Remove the selected item from the Mealsid array
+ const index = Mealsid.indexOf(id);
+ if (index > -1) {
+   Mealsid.splice(index, 1);
+ }
+
+ // Update local storage with the updated Mealsid array
+ localStorage.setItem("Mealsid", JSON.stringify(Mealsid));
+
+ const listItem = document.getElementById(id);
+ console.log(listItem);
+ if (listItem) {
+  alert('item removed from the favourite list')
+   listItem.remove();
+ }
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,68 @@
+let search = document.getElementById("search");
+let button = document.getElementById("btn");
+let container = document.getElementById("container");
+let display = document.getElementById("meal-list");
+let meals = [];
+
+// adding the keyup evnet on the search bar
+search.addEventListener("keyup", getMealList);
+
+// adding the click event on the button
+button.addEventListener("click", getMealList);
+
+// doing the work on the getMealList function
+async function getMealList() {
+  let searchInput = search.value;
+  let res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+  );
+  let data = await res.json();
+  // console.log(data);
+  let mealString = data.meals
+    .map((meal) => {
+      if (searchInput == "") {
+        display.innerHTML = "";
+      } else {
+        return `
+        <li id="${data.meals[0].idMeal}" class="list">
+        <div id="list-part">
+                <div class="img"><img src="${meal.strMealThumb}"/>
+                </div>
+                <div class="name"> ${meal.strMeal}
+                </div>
+                <a href="recipe.html?id=${meal.idMeal}" style="color:white" >Details</a>
+                <button class="fav-btn" onclick="addfavItem(${meal.idMeal})">add favourite</button>
+                </div>
+        </li>`;
+      }
+    })
+    .join("");
+  display.innerHTML = mealString;
+
+
+  // working on the button when click the text and the color changed and when again click it's comes on the previous look
+  let list = document.querySelectorAll(".fav-btn");
+  
+  list.forEach((btn) => {
+  
+    btn.addEventListener("click", () => {
+     
+        btn.style.backgroundColor = "red";
+        btn.innerHTML = "favourite";
+        alert("item added to favourite successfully!!");
+        mark = false;
+      
+    });
+  });
+}
+
+// adding the favourite item to the favourite part
+let fav = [];
+
+function addfavItem(id) {
+  fav.push(id);
+  localStorage.setItem("Mealsid", JSON.stringify(fav));
+  
+  // console.log("this is fav arr",fav);
+
+}
